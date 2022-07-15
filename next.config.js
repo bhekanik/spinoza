2a4a8d3d5679b67587bb33d1347404aaf1d2acaa
Bhekani Khumalo <bhekanik@gmail.com>
2022-07-15 13:17:05 +0100

    Add PWA stuff

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const withPWA = require("next-pwa");
+
+const moduleExports = {
+  reactStrictMode: true,
+  optimization: {
+    usedExports: true,
+  },
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+  swcMinify: true,
+  future: {
+    webpack5: true,
+  },
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withPWA(moduleExports);
