5e8b5fba3810a6ca78045d4202ef14d63f47de56
Guide Fari <guideg6@gmail.com>
2022-07-15 06:28:28 +0200

    component: upload file

---
+++ src/components/UploadFile.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react"
+
+export const UploadFile = () => {
+  const [selectedFile, setSelectedFile] = useState<File>()
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files?.[0]) {
+      setSelectedFile(event.target.files[0])
+    }
+  }
+
+  return (
+    <div className="relative flex mb-4 space-x-3 ">
+      <input
+        type="file"
+        name="file"
+        className="w-full max-w-md px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
+        onChange={changeHandler}
+      />
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from "next"
 import { ArticleList } from "../components/ArticleList"
 import { InputLink } from "../components/InputLink"
 import { SelectLanguage } from "../components/SelectLanguage"
+import { UploadFile } from "../components/UploadFile"
 
 const Home: NextPage = () => {
   return (
     <main className="px-6 py-10">
       <SelectLanguage />
       <InputLink />
+      <UploadFile />
       <ArticleList />
     </main>
   )
