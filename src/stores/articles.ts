28acf730ed3c0ea9474f1b0e28e06965f8475ee4
Bhekani Khumalo <bhekanik@gmail.com>
2022-07-17 18:54:55 +0100

    Add devtools

--- src/stores/articles.ts
+++ src/stores/articles.ts
@@ -1,7 +1,7 @@
 import { Article } from "src/lib/parseUrl";
 import { SynthRequest } from "src/pages/api/all-synth";
 import create from "zustand";
-
+import { devtools } from "zustand/middleware";
 interface ArticlesState {
   articles: Article[];
   addArticle: (article: Article) => void;
@@ -12,72 +12,74 @@ interface ArticlesState {
   setStatusFromSynthRequests: (synthRequests: SynthRequest[]) => void;
 }
 
-export const useStore = create<ArticlesState>((set) => ({
-  articles: [],
-  deleteArticle: (id: string) =>
-    set((state) => {
-      const newState = state.articles.filter((article) => article.id !== id);
-      window.localStorage.setItem("articles", JSON.stringify(newState));
-      return {
-        articles: newState,
-      };
-    }),
-  addArticles: (articles: Article[]) =>
-    set(() => {
-      const newState = [...articles];
-      window.localStorage.setItem("articles", JSON.stringify(newState));
-      return {
-        articles: newState,
-      };
-    }),
-  addArticle: (article: Article) =>
-    set((state) => {
-      const newState = [...state.articles, article];
-      window.localStorage.setItem("articles", JSON.stringify(newState));
-      return {
-        articles: newState,
-      };
-    }),
-  setStatusFromSynthRequests: (synthRequests: SynthRequest[]) =>
-    set((state) => {
-      const newState = state.articles.map((article: Article) => {
-        let newArticle = { ...article };
-        synthRequests.forEach((synthRequest) => {
-          if (article.id === synthRequest.id) {
-            newArticle = { ...article, status: synthRequest.status };
-          }
+export const useStore = create(
+  devtools<ArticlesState>((set) => ({
+    articles: [],
+    deleteArticle: (id: string) =>
+      set((state) => {
+        const newState = state.articles.filter((article) => article.id !== id);
+        window.localStorage.setItem("articles", JSON.stringify(newState));
+        return {
+          articles: newState,
+        };
+      }),
+    addArticles: (articles: Article[]) =>
+      set(() => {
+        const newState = [...articles];
+        window.localStorage.setItem("articles", JSON.stringify(newState));
+        return {
+          articles: newState,
+        };
+      }),
+    addArticle: (article: Article) =>
+      set((state) => {
+        const newState = [...state.articles, article];
+        window.localStorage.setItem("articles", JSON.stringify(newState));
+        return {
+          articles: newState,
+        };
+      }),
+    setStatusFromSynthRequests: (synthRequests: SynthRequest[]) =>
+      set((state) => {
+        const newState = state.articles.map((article: Article) => {
+          let newArticle = { ...article };
+          synthRequests.forEach((synthRequest) => {
+            if (article.id === synthRequest.id) {
+              newArticle = { ...article, status: synthRequest.status };
+            }
+          });
+          return newArticle;
         });
-        return newArticle;
-      });
-      window.localStorage.setItem("articles", JSON.stringify(newState));
-      return {
-        articles: newState,
-      };
-    }),
-  setStatus: (url: string, status: string) =>
-    set((state) => {
-      const newState = state.articles.map((article: Article) => {
-        if (article.synthStatusUrl === url) {
-          return { ...article, status };
-        }
-        return article;
-      });
-      window.localStorage.setItem("articles", JSON.stringify(newState));
-      return {
-        articles: newState,
-      };
-    }),
-  setDownloadUrl: (id: string, downloadUrl: string) =>
-    set((state) => {
-      const newState = state.articles.map((article: Article) => {
-        if (article.id === id) {
-          return { ...article, downloadUrl };
-        }
-        return article;
-      });
-      window.localStorage.setItem("articles", JSON.stringify(newState));
-      return {
-        articles: newState,
-      };
-    }),
-}));
+        window.localStorage.setItem("articles", JSON.stringify(newState));
+        return {
+          articles: newState,
+        };
+      }),
+    setStatus: (url: string, status: string) =>
+      set((state) => {
+        const newState = state.articles.map((article: Article) => {
+          if (article.synthStatusUrl === url) {
+            return { ...article, status };
+          }
+          return article;
+        });
+        window.localStorage.setItem("articles", JSON.stringify(newState));
+        return {
+          articles: newState,
+        };
+      }),
+    setDownloadUrl: (id: string, downloadUrl: string) =>
+      set((state) => {
+        const newState = state.articles.map((article: Article) => {
+          if (article.id === id) {
+            return { ...article, downloadUrl };
+          }
+          return article;
+        });
+        window.localStorage.setItem("articles", JSON.stringify(newState));
+        return {
+          articles: newState,
+        };
+      }),
+  }))
+);
